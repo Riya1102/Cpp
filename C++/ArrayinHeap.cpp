e195b0ac4e895601b78930f7952e12a4f5ef754c
Riya1102 <rkriyakumari1302@gmail.com>
2021-08-04 09:44:24 +0530

    Edits

---
+++ C++/ArrayinHeap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int *p=new int[5];
+    p[0]= 12;
+    p[1]= 13;
+
+    cout<<p[0]<<endl;
+
+    delete []p;
+    p= nullptr;
+
+    return 0;
+
+}
